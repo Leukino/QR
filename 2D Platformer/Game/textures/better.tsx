ab4d825fb0012a49d43eff30c02921ac54124f8c
Paideieitor <45202190+Paideieitor@users.noreply.github.com>
2019-10-14 15:33:42 +0200

    map R updates,

---
+++ 2D Platformer/Game/textures/better.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="better" tilewidth="16" tileheight="16" tilecount="230" columns="23">
+ <image source="TexturesEpicMergeuvepuntodos.png" width="368" height="168"/>
+</tileset>
