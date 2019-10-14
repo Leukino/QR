7145758ba1f0668f9538ee596b565ddf78bbb28b
Paideieitor <45202190+Paideieitor@users.noreply.github.com>
2019-10-14 15:01:47 +0200

    cossas

---
+++ 2D Platformer/Game/textures/wardu el conjunto de patrones aqui perque no  se on ficarlo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Castle" tilewidth="16" tileheight="16" tilecount="210" columns="21">
+ <image source="TexturesEpicMerge.png" width="336" height="168"/>
+</tileset>
