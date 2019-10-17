2ce6db245b208d74de85a1888f00976bb8ce998a
Señor González <miquelqg@TTC.CAT>
2019-10-17 13:35:32 +0200

    .

--- 2D Platformer/Motor2D/j1Map.cpp
+++ 2D Platformer/Motor2D/j1Map.cpp
@@ -39,25 +39,26 @@ void j1Map::Draw()
 
 	
 	p2List_item<layer*>* item_layer = data.layers.start;
-	layer* l = item_layer->data;
-	while (item_layer->next != NULL)
-	{
-		layer* l = item_layer->data;
 		// TODO 5: Prepare the loop to iterate all the tiles in a layer
 		for (int y = 0; y < data.height; y++) {
 			for (int x = 0; x < data.width; x++) {
-
+				while (item_layer != NULL)
+				{
 				//data.layers[0]->Get(x, y); //returns index of the position
-
 				//Idtile.attribute("gid").as_uint() -1) //data.tilesets.start->data->GetRect(Idtile.attribute("gid").as_uint() - 1)
-				App->render->Blit(data.tilesets.start->data->texture, GetPosX(x), GetPosY(y), data.tilesets.start->data->GetRect(l->tile_ids[l->Get(x, y)] - 1)); //finally get the tile id
+				App->render->Blit(data.tilesets.start->data->texture, 
+					GetPosX(x), 
+					GetPosY(y), 
+					data.tilesets.start->data->GetRect(item_layer->data->tile_ids[item_layer->data->Get(x, y)] - 1));//finally get the tile id
+				LOG("ended all layers");
+				item_layer = item_layer->next;
+				LOG("bool %s", item_layer ? "true" : "false");
+				}
 			}
-			LOG("ended layer");
+			//LOG("ended layer");
 		}
-		LOG("ended all layers");
-		item_layer = item_layer->next;
-		LOG("bool %s", l ? "true" : "false");
-	}
+		
+	
 	id = 0; 
 	// TODO 9: Complete the draw function
 }
@@ -142,7 +143,6 @@ bool j1Map::Load(const char* file_name)
 		{
 			ret = LoadLayer(layerset, newLayer);
 		}
-		
 		data.layers.add(newLayer);
 	}
 
