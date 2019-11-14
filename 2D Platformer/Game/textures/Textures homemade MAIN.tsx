fc08797bc4b6ad96d46695755f4a8640e793fb8c
Paideieitor <45202190+Paideieitor@users.noreply.github.com>
2019-11-14 21:13:02 +0100

    New map texture added

---
+++ 2D Platformer/Game/textures/Textures homemade MAIN.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Textures homemade" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="Tileset que no es basura or so I hope.png" width="160" height="160"/>
+</tileset>
